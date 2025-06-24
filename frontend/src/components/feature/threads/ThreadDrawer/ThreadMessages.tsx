fea14abc12648592c55a5174f26362843807da0d
phamducbinhh <phamducbinh1712000@gmail.com>
2025-06-24 06:59:48 +0000

    Update Thread Realtime

--- frontend/src/components/feature/threads/ThreadDrawer/ThreadMessages.tsx
+++ frontend/src/components/feature/threads/ThreadDrawer/ThreadMessages.tsx
@@ -12,7 +12,7 @@ import { VirtuosoHandle } from 'react-virtuoso'
 import { Message } from '../../../../../../types/Messaging/Message'
 import AIEvent from '../../ai/AIEvent'
 import useFileUpload from '../../chat/ChatInput/FileInput/useFileUpload'
-import Tiptap from '../../chat/ChatInput/Tiptap'
+import TiptapThread from '../../chat/ChatInput/Tiptap_Thread'
 import TypingIndicator from '../../chat/ChatInput/TypingIndicator/TypingIndicator'
 import { useTyping } from '../../chat/ChatInput/TypingIndicator/useTyping'
 import { useSendMessage } from '../../chat/ChatInput/useSendMessage'
@@ -183,7 +183,7 @@ export const ThreadMessages = ({ threadMessage }: { threadMessage: Message }) =>
         {isUserInChannel && (
           <Stack>
             <TypingIndicator channel={threadID ?? ''} />
-            <Tiptap
+            <TiptapThread
               key={threadID}
               channelID={threadID}
               fileProps={{
